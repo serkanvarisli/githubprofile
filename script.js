45bfd3ff292f2579ffd028ee7421e834c0b75475
Serkan Varışlı <87394796+serkanvarisli@users.noreply.github.com>
2023-04-06 19:54:07 +0300

    Upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const APIURL = "https://api.github.com/users/";
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const main = document.getElementById("main");
+
+async function getUser(username) {
+  const { data } = await axios(APIURL + username);
+  createUserCard(data);
+}
+
+function createUserCard(user) {
+  const cardHTML = `
+   
+  <a href="https://github.com/${user.login}" target="_blank=">
+  <div class="card">
+    <div>
+    <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
+    </div>
+    <div class="user-info">
+        <h2>${user.name}</h2>
+        <p>${user.bio}</p>
+        <ul>
+            <li>${user.followers}<strong>Followers</strong></li>
+            <li>${user.following}<strong>Following</strong></li>
+            <li>${user.public_repos}<strong>Repos</strong></li>
+        </ul>
+    </div>
+</div>
+</a>
+    `;
+  main.innerHTML = cardHTML;
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const user = search.value;
+  if (user) {
+    getUser(user);
+    search.value = "";
+  }
+});
